1c8d901705b12753410a317172fd730d82eb981d
Cassy Omondi <cksparks07@gmail.com>
2025-06-24 23:48:24 +0300

    Created variables in script.js linked to existing HTML elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const container = document.getElementById('countries-container');
+const modal = document.getElementById('country-modal');
+const modalDetails = document.getElementById('modal-details');
+const closeBtn = document.querySelector('.close-button');
